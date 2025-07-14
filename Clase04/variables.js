52d3a6f36a23187a669ba38c0eafb12d7f8fbdb4
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-14 17:30:35 -0500

    Clase 04: Variables y buenas practicas

---
+++ Clase04/variables.js
@@ -0,0 +1,27 @@
+// Variables con let
+
+let caja = 'woody';
+console.log(caja)
+
+// Lo NO permitido
+
+let c = 'woody';
+let ca,a,b 
+
+// Lo permitido
+
+let primerTrasteoDePepito ='woody';
+let urlDelUsuario = 'https:/www.google.com'
+let idDelUsuario = '1234'
+
+//lo recomendable es definir las variables antes que todo 
+
+// se usa let se utiliza para variables con cosas que cambian con el tiempo, ejemplo:
+
+let contador 
+contador = contador + 1
+
+// const se usa para variable contantes o fijas
+
+const pi = 3.14 
+
