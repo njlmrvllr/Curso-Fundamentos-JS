a86da80602bbbe905fbb4fd9977ef6374a03ea94
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-14 17:46:18 -0500

    Clase 05: Tipos de datos en JavaScript

---
+++ Clase05/TiposDeDatos.md
@@ -0,0 +1,14 @@
+
+Existen 10 tipos de datos 
+
+|*Primitivos*   |*Complejos*   |
+|:-:|:-:|
+|string|object   |   
+| number  |array   |   
+| boolean  | function   |
+|  null |   |
+|  undefined |   |
+|  symbol |   |
+| bigint |   |
+
+
---
+++ Clase05/tipos-de-datos.js
@@ -0,0 +1,38 @@
+// PRIMITIVOS
+
+// string 
+let nombre = 'Tere'
+
+//number
+let edad = 25
+
+//boolean 
+let esMayorDeEdad = true
+
+// null
+let noHayValor = null
+
+// undefined
+let noDefinido = undefined //normalmente js lo asigna
+
+// symbol
+let simboloUnico = Symbol('único')
+
+//bigint 
+let numeroGrande = 2n
+
+
+// COMPLEJOS 
+
+//object 
+let carro = {
+    marca: 'tesla',
+    modelo: 'model s'
+}
+
+// arrays 
+let frutas = ['manzana','platano','uvas']
+
+//function
+function saludar () {}
+
