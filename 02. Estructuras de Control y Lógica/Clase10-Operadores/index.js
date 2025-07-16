b30dd4cd5a0383080e3c654badbd76313470c7fc
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-16 17:21:18 -0500

    Clase 10: Operadores de Comparación en JavaScript: Igualdad y desigualdad

---
+++ 02. Estructuras de Control y Lógica/Clase10-Operadores/index.js
@@ -0,0 +1,20 @@
+// Operadores de comparación
+
+==   //ayudan a comparar si algo es igual a otra cosa
+===  //si algo es igual a algo, tanto en valor como tipo
+!=   //si es diferente de 
+!==  //si es diferente de - en valor y tipo de dato
+>
+<
+>= 
+<= //</>
+
+const a = 10;
+const b = 20;
+const c = '10'
+
+a == b; //false
+a === c //false
+a != b //true
+a !== c //true
+
