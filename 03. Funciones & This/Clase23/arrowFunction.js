d57d8515045e2663c6d6bafe06d9636fdff3dae3
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-18 01:18:00 -0500

    Clase 23: Arrow Functions

---
+++ 03. Funciones & This/Clase23/arrowFunction.js
@@ -0,0 +1,26 @@
+const greeting = function (name) {
+    return `Hi, ${name}`
+}
+
+// Arrow function - explicit return
+const newGreeting =  (name) => {
+    return `Hi, ${name}`
+}
+
+// Arrow function - implicit return
+const newGreetingImplicit = name => `hi, ${name}`
+const newGreetingImplicitWithTwoParameter = (name,lastName) => `hi, I'm ${name} ${lastName}`
+
+//Lexical Binding
+const fictionalCharacter = {
+    name: 'Uncle Ben',
+    messageWithTradionalFunction: function(message){
+        console.log(`${this.name} says: ${message}`)},
+    messageWithArrowFunction: (message) => {
+        console.log(`${this.name} says: ${message}`)
+    }
+}
+
+
+fictionalCharacter.messageWithTradionalFunction('With great power comes great responsability')
+fictionalCharacter.messageWithArrowFunction('Beware of Doctor Octopus')
