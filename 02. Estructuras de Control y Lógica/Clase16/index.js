312289219b5bf093b972d0e48ae682c27f9734a0
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-17 21:43:53 -0500

    Clase 16: Iteración for-of

---
+++ 02. Estructuras de Control y Lógica/Clase16/index.js
@@ -0,0 +1,13 @@
+// For of arrays, strigs [algo]
+// utilizado para objetos iterables
+/*
+for (variable del objeto){
+    codigo
+}
+*/
+
+let canasta = ['manzana', 'pera', 'naranja', 'uva'];
+
+for (fruta of canasta){
+    console.log(fruta);
+}
