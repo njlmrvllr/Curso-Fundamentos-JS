7baacd6327d72ad7498ca747fd308800d6b615a2
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-17 21:15:31 -0500

    Clase 14: Uso de Switch

---
+++ 02. Estructuras de Control y Lógica/Clase14/index.js
@@ -0,0 +1,12 @@
+switch(expresion)
+{
+    case valor1:
+        //codigo a ejecutar
+        break;
+    case valor2: 
+        //codigo a ejecutar 
+        break;
+    /// ... 
+    default: 
+    /// ... 
+}
