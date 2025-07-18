6e60501e1c2f065699e808475a9879b5c263c764
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-17 23:46:40 -0500

    Clase 21: Diferencias entre Funciones y Métodos en JavaScript

---
+++ 03. Funciones & This/Clase21/function-metod.js
@@ -0,0 +1,44 @@
+//Capacidades que tienen las funciones al igual que otros 
+
+// 1. pasar funciones como argumentos -> callback
+
+function a(){}
+function b(a){}
+b(a)
+
+// Retornar funciones 
+function a(){
+    function b(){}
+    return b
+}
+
+//Asignar funciones a variables -> Expresión de funcion
+const a = function(){}
+
+// Tener propiedades y métodos
+function a() {}
+const obj = {}
+a.call(obj)
+
+//Anidar funciones -> Nested functions
+function a(){
+    function b (){
+        function c(){
+
+        }
+        c()
+    }
+    b()
+}
+a()
+
+// Es posible almacenar funciones en objetos? 
+
+const rocket = {
+    name: 'Falcon 9',
+    launchMessage: function launchMessage (){
+        console.log('🔥')
+    }
+}
+
+rocket.launchMessage()
