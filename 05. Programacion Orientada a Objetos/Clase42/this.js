d895c6d1290ba229554ae9d9f9dfdef8cdba272a
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-22 17:29:15 -0500

    Clase 42: Uso de this

---
+++ 05. Programacion Orientada a Objetos/Clase42/this.js
@@ -0,0 +1,19 @@
+/*
+this --- class
+this --- objeto --- class
+*/
+
+class Persona {
+    constructor(nombre,edad){
+        this.nombre = nombre 
+        this.edad = edad
+    }
+}
+
+const persona1 = new Persona('Alice', 25)
+
+console.log(persona1);
+
+persona1.nuevoMetodo = function(){
+    console.log(`mi nombre es ${this.nombre}`);
+}
