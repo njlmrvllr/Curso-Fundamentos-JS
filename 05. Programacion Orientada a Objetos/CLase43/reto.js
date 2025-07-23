f4ede399d051f0246192a5496cee57495ecfa6c4
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-22 21:21:08 -0500

    Clase 43: Validacion de usuarios - algoritmo basico

---
+++ 05. Programacion Orientada a Objetos/CLase43/reto.js
@@ -0,0 +1,55 @@
+/*
+Requerimientos del reto: 
+1. El usuario debe poder ingresar su usuario y contrasenña
+2. El sistea debe ser capaz de validar si el usuario y contraseña ingresados por el usuario existen en la base de datos
+3. Si el usuario y contraseña son correctos, el sistema debe mostrar un mensaje de bienvenida y mostrar el timeline del usuario
+4. Si el usuario y contraseña son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningun timeline
+*/
+
+const userDatabase = [
+    {
+        username: 'andres',
+        password: '123',
+    },
+    {
+        username: 'caro',
+        password: '456',
+    },
+    {
+        username: 'mariana',
+        password: '789',
+    },
+]
+const userTimeline = [
+    {
+        username: 'Estefany',
+        timeline: 'Me encanta javascript',
+    },
+    {
+        username: 'Oscar',
+        timeline: 'Bebeloper es lo mejor',
+    },
+    {
+        username: 'Mariana',
+        timeline: 'A mi me gusta mas el cafe que el te',
+    },
+    {
+        username: 'Andres',
+        timeline: 'yo hoy no quiero trabajar',
+    },
+]
+
+const username = prompt('Cual es tu usuario?')
+const password = prompt('Cual es tu contraseña?')
+
+function usuarioExistente(username,password){
+    for(let i = 0; i < userDatabase.length; i++){
+        if(userDatabase[i].username === username && userDatabase[i].password === password){
+            console.log('Es correcto')
+        } else {
+            console.log('No es correcto')
+        }
+    }
+}
+
+usuarioExistente(username,password)
