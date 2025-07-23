da67276af5046bb04118a22cf5898821f39330ee
njlmrvllr <anjaliamarovillar@outlook.com>
2025-07-23 01:03:25 -0500

    Clase 47: Uso de Async/Await para Promesas Asincronas en JS

---
+++ 06. Asincronia en JavaScript/Clase47/app.js
@@ -0,0 +1,16 @@
+    // function fetchData () {
+    //     fetch("https://rickandmortyapi.com/api/character")
+    //     .then((response) => response.json())
+    //     .then((data)=> console.log(data))
+    //     .catch((error) => console.log(error))
+    // }
+
+async function fetchData() {
+    try {
+        let response = await fetch("https://rickandmortyapi.com/api/character")
+        let data = await response.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
